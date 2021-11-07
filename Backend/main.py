f0b8440d4c274a63351435fe5b110c4d77b72f5d
Paweł Frąckiewicz <pfrackiewicz1999@gmail.com>
2021-11-07 10:33:07 +0100

    Made drawing mock data go through the socket

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,7 @@
 import asyncio
 import socketio
 import uvicorn
+import random
 
 sio = socketio.AsyncServer(cors_allowed_origins='*', async_mode='asgi')
 app = socketio.ASGIApp(sio)
@@ -12,6 +13,15 @@ async def connect(sid, environ):
 @sio.event
 def disconnect(sid):
     print('disconnect ', sid)
+
+@sio.event
+async def start_simulation(sid, data):
+    print(sid)
+    print(data)
+    ret = {}
+    for i in range(int(data)):
+        ret[i] = [random.random(), random.random()]
+    await sio.emit('simulation_finished', ret)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=5000, log_level="info")

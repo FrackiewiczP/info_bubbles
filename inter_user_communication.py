be2273299d2631c9bdfd48ed35ed5b06bc0d8072
Jasiek Janiszewski <jasiekj1@gmail.com>
2021-11-17 16:35:03 +0100

    type hinting and typo in finter_user_communication fix

--- inter_user_communication.py
+++ inter_user_communication.py
@@ -4,21 +4,26 @@ import information
 
 
 class InterUserCommunication:
-    def __init__(self,users:{}):
+    def __init__(self, users: dict):
         self.users = users
-    def send_info_to_friends(self,frends:[],info:information):
+
+    def send_info_to_friends(self, friends: dict, info: information):
         pass
 
+
 class ToAllCommunication(InterUserCommunication):
-    def __init__(self, users: {}):
+    def __init__(self, users: dict):
         super(ToAllCommunication, self).__init__(users)
-    def send_info_to_friends(self,frends:[],info:information):
-        for friend in frends:
+
+    def send_info_to_friends(self, friends: list, info: information):
+        for friend in friends:
             self.users[friend].try_to_integrate_info_bit(info)
 
+
 class ToOneRandomCommunication(InterUserCommunication):
-    def __init__(self, users: {}):
+    def __init__(self, users: dict):
         super(ToOneRandomCommunication, self).__init__(users)
-    def send_info_to_friends(self,frends:[],info:information):
-        friend = random.choice(frends)
+
+    def send_info_to_friends(self, friends: list, info: information):
+        friend = random.choice(friends)
         self.users[friend].try_to_integrate_info_bit(info)

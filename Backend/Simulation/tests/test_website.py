68e0a2aaee00015192dc862cd2b61f349eda093e
j-janiszewski <37154224+j-janiszewski@users.noreply.github.com>
2021-12-05 13:47:34 +0100

    comment fix

--- Backend/Simulation/tests/test_website.py
+++ Backend/Simulation/tests/test_website.py
@@ -8,7 +8,7 @@ def test_creating_initial_connections_random__non_directed():
         no_of_users, no_of_links
     )
     # It is possible for certain no_of_links and no_of_users
-    # that one user will no_of_links - 1
+    # that one user will have (no_of_links - 1) friends
     assert abs(len(links) - (no_of_users * no_of_links) / 2) <= 1
     for user in users_friends:
         assert len(users_friends[user]) == no_of_links

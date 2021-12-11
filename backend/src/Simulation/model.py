75549a078802c9b4d30c7bf3ff2996f3bfba2858
Paweł Frąckiewicz <pfrackiewicz1999@gmail.com>
2021-12-11 18:31:27 +0100

    Fix after rebase

--- backend/src/Simulation/model.py
+++ backend/src/Simulation/model.py
@@ -8,11 +8,14 @@ List of models:
 
 """
 
+from time import time
+import tracemalloc
 import numpy as np
 import pandas as pd
 from mesa import Model
 from Simulation.website import Website
 from Simulation.user_agent import UserAgent
+from DatabaseConnection.database_connector import DatabaseConnector
 
 
 class TripleFilterModel(Model):
@@ -23,40 +26,47 @@ class TripleFilterModel(Model):
 
     def __init__(
         self,
-        num_of_users,
-        communication_form="individual",
-        latitude_of_acceptance=0.5,
-        sharpness_parameter=20,
-        memory_size=10,
+        number_of_agents,
+        number_of_steps,
         number_of_links=10,
-        link_delete_prob=0.01,
+        mem_capacity=10,
+        friend_lose_prob=0.01,
+        communication_form="individual",
         inter_user_communication_form="to_one_random",
+        acc_latitude=0.5,
+        acc_sharpness=20,
+
         initial_connections="random",
         sd_of_user_latitudes=0.2,
+        db_connector: DatabaseConnector=None,
+        socket_id=None,
     ):
 
-        self.num_of_users = num_of_users
-        self.latitude_of_acceptance = latitude_of_acceptance
-        self.sharpness_parameter = sharpness_parameter
-        self.memory_size = memory_size
+        self.number_of_agents = number_of_agents
+        self.number_of_steps = number_of_steps
+        self.db_connector = db_connector
+        self.socket_id=socket_id
+        self.acc_latitude = acc_latitude
+        self.acc_sharpness = acc_sharpness
+        self.mem_capacity = mem_capacity
         self.number_of_links = number_of_links
-        self.link_delete_prob = link_delete_prob
+        self.friend_lose_prob = friend_lose_prob
         self.iterations = 0
         self.user_positions_in_prev = {}
         users = {}
         user_positions = {}
 
         user_latitudes = np.random.normal(
-            self.latitude_of_acceptance, sd_of_user_latitudes, size=self.num_of_users
+            self.acc_latitude, sd_of_user_latitudes, size=self.number_of_agents
         )
 
-        for i in range(self.num_of_users):
+        for i in range(self.number_of_agents):
             a = UserAgent(
                 i,
                 self,
-                self.memory_size,
+                self.mem_capacity,
                 user_latitudes[i],
-                self.sharpness_parameter,
+                self.acc_sharpness,
             )
             users[i] = a
 
@@ -65,7 +75,7 @@ class TripleFilterModel(Model):
         self.website = Website(
             users,
             number_of_links,
-            link_delete_prob,
+            friend_lose_prob,
             initial_connections,
             communication_form,
             inter_user_communication_form,
@@ -76,7 +86,16 @@ class TripleFilterModel(Model):
 
     def step(self):
         self.iterations += 1
-        self.user_positions_in_prev[self.iterations] = self.website.step()
+
+        # website.step returns a dict, where
+        # key - userId
+        # value - 1x2 numpy array with user's position
+        current_step_data = self.website.step()
+
+        # MongoDB only allows strings as document keys
+        current_step_data_with_string_keys = {str(key): list(value) for (key,value) in current_step_data.items()}
+        self.user_positions_in_prev[self.iterations] = current_step_data
+        self.db_connector.save_simulation_step(self.socket_id,self.iterations, current_step_data_with_string_keys)
 
     def save_output(self):
         df = pd.DataFrame.from_dict(self.user_positions_in_prev, orient="index")
@@ -84,7 +103,7 @@ class TripleFilterModel(Model):
         df["x_pos"] = df.position.apply(lambda x: x[0])
         df["y_pos"] = df.position.apply(lambda x: x[1])
         df = df.drop(["position"], axis=1)
-        df["step"] = list(range(self.iterations + 1)) * self.num_of_users
+        df["step"] = list(range(self.iterations + 1)) * self.number_of_agents
         df.to_csv("positions.csv")
     
     def get_output(self):
@@ -93,3 +112,16 @@ class TripleFilterModel(Model):
             ret[step] = {key: value.tolist() for key, value in self.user_positions_in_prev[step].items()}
         return ret
 
+    def run_simulation(self):
+        start_time = time()
+        self.db_connector.delete_previous_simulation_of_socket(self.socket_id)
+        tracemalloc.start()
+        for i in range(self.number_of_steps):
+            print("--------------------" + str(i) + "----------------")
+            self.step()
+        print("total  --- %s seconds ---" % (time() - start_time))
+        current, peak = tracemalloc.get_traced_memory()
+        print(f"Peak memory usage was {peak / 10 ** 6} MB")
+        tracemalloc.stop()
+        # self.save_output()
+        # return self.get_output()
--- backend/src/main.py
+++ backend/src/main.py
@@ -4,18 +4,16 @@ import uvicorn
 import random
 from pymongo import MongoClient
 
-from Simulation.simulation import Simulation
+from DatabaseConnection.database_connector import DatabaseConnector
+from DatabaseConnection.database_connector import CONNECTION_STRING, COLLECTION_NAME, DATABASE_NAME
+from Simulation.model import TripleFilterModel
 
 sio = socketio.AsyncServer(cors_allowed_origins='*', async_mode='asgi')
 app = socketio.ASGIApp(sio)
-client = MongoClient("mongodb://mongodb:27017")
-simulations = {}
+db_reader = DatabaseConnector(CONNECTION_STRING, DATABASE_NAME, COLLECTION_NAME)
 
-# Test connection to db
-# print(client.server_info())
-
-def parse_data_from_frontend(data):
-    return Simulation(
+def parse_data_from_frontend(socket_id, data):
+    return TripleFilterModel(
         number_of_agents=data["number_of_agents"],
         number_of_steps=data["number_of_steps"],
         number_of_links=data["number_of_links"],
@@ -24,33 +22,14 @@ def parse_data_from_frontend(data):
         communication_form=data["communication_form"],
         inter_user_communication_form=data["inter_user_communication_form"],
         acc_latitude=data["acc_latitude"],
-        acc_sharpness=data["acc_sharpness"]
+        acc_sharpness=data["acc_sharpness"],
+        db_connector=DatabaseConnector(CONNECTION_STRING, DATABASE_NAME, COLLECTION_NAME),
+        socket_id=socket_id,
     )
 
-def perform_simulation(data):
-    sim = parse_data_from_frontend(data)
-    res = sim.run_simulation()
-    return res
-
-def perform_mock_simulation(num_of_users, num_of_steps):
-    result = []
-    for i in range(num_of_steps):
-        curr_step = {}
-        if i == 0:
-            for j in range(num_of_users):
-                curr_step[j] = [random.random()*2-1, random.random()*2-1]
-        else:
-            for j in range(num_of_users):
-                curr_step[j] = [result[i-1][j][0]+get_random_step(), result[i-1][j][1]+get_random_step()]
-        result.append(curr_step)
-    return result
-
-def get_random_step():
-    r = random.random()
-    if(r < 0.5):
-        return -0.01
-    else:
-        return 0.01
+def perform_simulation(socket_id, data):
+    model = parse_data_from_frontend(socket_id, data)
+    model.run_simulation()
 
 @sio.event
 async def connect(sid, environ):
@@ -64,8 +43,14 @@ def disconnect(sid):
 async def start_simulation(sid, data):
     print(sid)
     print(data)
-    result = perform_simulation(data)
-    await sio.emit('simulation_finished', [result, data["number_of_steps"]], room=sid)
+    perform_simulation(sid, data)
+    await sio.emit('simulation_finished', data["number_of_steps"], room=sid)
+
+@sio.event
+async def simulation_step_requested(sid, step_num):
+    step = db_reader.get_simulation_step(sid, int(step_num))
+    print(f"Socket {sid} requested step {step_num}")
+    await sio.emit("simulation_step_sent", step)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info")

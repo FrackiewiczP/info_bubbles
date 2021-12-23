1d81206bcbc7dd5d32c5174ebcda755e865852c6
j-janiszewski <37154224+j-janiszewski@users.noreply.github.com>
2021-12-23 16:50:32 +0100

    Update test_friends_links.py

--- backend/src/tests/test_friends_links.py
+++ backend/src/tests/test_friends_links.py
@@ -5,7 +5,7 @@ def test_creating_initial_connections_random__non_directed():
     no_of_links = 8
     no_of_users = 200
     links, users_friends = FriendLinks.create_random_non_directed_friends_links(
-        no_of_users, no_of_links
+        list(range(200)), no_of_links
     )
     # It is possible for certain no_of_links and no_of_users
     # that one user will have (no_of_links - 1) friends

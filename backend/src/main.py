c7c093693a62dc72c4b17f94de414ddfa2b802f4
j-janiszewski <37154224+j-janiszewski@users.noreply.github.com>
2021-12-27 12:36:42 +0100

    Update main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,25 +5,30 @@ import random
 from pymongo import MongoClient
 
 from DatabaseConnection.database_connector import DatabaseConnector
-from DatabaseConnection.database_connector import CONNECTION_STRING, COLLECTION_NAME, DATABASE_NAME
+from DatabaseConnection.database_connector import (
+    CONNECTION_STRING,
+    COLLECTION_NAME,
+    DATABASE_NAME,
+)
 from Simulation.model import TripleFilterModel
 from Simulation.simulation_runner import SimulationRunner
 
-sio = socketio.AsyncServer(cors_allowed_origins='*', async_mode='asgi')
+sio = socketio.AsyncServer(cors_allowed_origins="*", async_mode="asgi")
 app = socketio.ASGIApp(sio)
 db_reader = DatabaseConnector(CONNECTION_STRING, DATABASE_NAME, COLLECTION_NAME)
 current_simulations = set()
 
+
 def parse_data_from_frontend(socket_id, data):
     model = TripleFilterModel(
-        number_of_agents=data["number_of_agents"],
+        num_of_users=data["number_of_agents"],
         number_of_links=data["number_of_links"],
-        mem_capacity=data["mem_capacity"],
-        friend_lose_prob=data["friend_lose_prob"],
+        memory_size=data["mem_capacity"],
+        link_delete_prob=data["friend_lose_prob"],
         communication_form=data["communication_form"],
         inter_user_communication_form=data["inter_user_communication_form"],
-        acc_latitude=data["acc_latitude"],
-        acc_sharpness=data["acc_sharpness"],
+        latitude_of_acceptance=data["acc_latitude"],
+        sharpness_parameter=data["acc_sharpness"],
     )
     return SimulationRunner(
         data["number_of_steps"],
@@ -31,10 +36,11 @@ def parse_data_from_frontend(socket_id, data):
         DatabaseConnector(CONNECTION_STRING, DATABASE_NAME, COLLECTION_NAME),
         sio,
         socket_id,
-        )
+    )
+
 
 async def perform_simulation(socket_id, data):
-    if(socket_id in current_simulations):
+    if socket_id in current_simulations:
         await sio.emit("simulation_already_running")
         return
     current_simulations.add(socket_id)
@@ -42,13 +48,16 @@ async def perform_simulation(socket_id, data):
     await model.run_simulation()
     current_simulations.remove(socket_id)
 
+
 @sio.event
 async def connect(sid, environ):
     print("connect ", sid)
 
+
 @sio.event
 def disconnect(sid):
-    print('disconnect ', sid)
+    print("disconnect ", sid)
+
 
 @sio.event
 async def start_simulation(sid, data):
@@ -56,10 +65,12 @@ async def start_simulation(sid, data):
     print(data)
     await perform_simulation(sid, data)
 
+
 @sio.event
 async def simulation_step_requested(sid, step_num):
     step = db_reader.get_simulation_step(sid, int(step_num))
     await sio.emit("simulation_step_sent", step)
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info")

42772099e1859634fbfb8e524b1bd73e75cb841a
Ignacy Sujecki <61029787+cangi75@users.noreply.github.com>
2021-12-31 13:26:14 +0100

    test fix

--- backend/src/tests/test_friends_links.py
+++ backend/src/tests/test_friends_links.py
@@ -4,18 +4,22 @@ from Simulation.friend_links import FriendLinks
 def test_creating_initial_connections_random__non_directed():
     no_of_links = 8
     no_of_users = 200
+    no_of_groups = 4
+    percent = 80
+
     links, users_friends = FriendLinks.create_random_non_directed_friends_links(
-        list(range(200)), no_of_links
+        list(range(200)), no_of_links, percent, no_of_groups
     )
-    # It is possible for certain no_of_links and no_of_users
-    # that one user will have (no_of_links - 1) friends
-    assert abs(len(links) - (no_of_users * no_of_links) / 2) <= 2
-    one_user_with_lesser_links_encountered = False
-    for user in users_friends:
-        if (
-            len(users_friends[user]) != no_of_links
-            and not one_user_with_lesser_links_encountered
-        ):
-            one_user_with_lesser_links_encountered = True
-            continue
-        assert len(users_friends[user]) == no_of_links
+
+    # is number of links ok
+    assert len(links) == no_of_links * no_of_users
+    for it in users_friends:
+        for it2 in users_friends[it]:
+            # there is no links to myself
+            assert it2 != it
+            # the links are non directed
+            non_direct = False
+            for it3 in users_friends[it2]:
+                if it3 == it:
+                    non_direct = True
+            assert non_direct

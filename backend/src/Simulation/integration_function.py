4ea1731bf64877694e20eb99c324961dcc2f8aac
j-janiszewski <37154224+j-janiszewski@users.noreply.github.com>
2022-01-04 21:05:59 +0100

    unfriending

--- backend/src/Simulation/integration_function.py
+++ backend/src/Simulation/integration_function.py
@@ -1,7 +1,21 @@
 import numpy as np
 
 
-def check_integration(position1, position2, latitude, sharpness):
+def integration_probability(position1, position2, latitude, sharpness):
     dist = np.linalg.norm(position1 - position2)
     probability = latitude ** sharpness / (dist ** sharpness + latitude ** sharpness)
-    return np.random.rand() <= probability
+    return probability
+
+
+def check_integration(position1, position2, latitude, sharpness):
+    return np.random.rand() <= integration_probability(
+        position1, position2, latitude, sharpness
+    )
+
+
+def check_link_integration(
+    position1, position2, latitude, sharpness, refriend_probability
+):
+    return np.random.rand() <= refriend_probability * (
+        1 - integration_probability(position1, position2, latitude, sharpness)
+    )

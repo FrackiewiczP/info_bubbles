148ad29c1924f53c0e03ff62448a4b5b6ce5e572
Paweł Frąckiewicz <pfrackiewicz1999@gmail.com>
2022-01-10 23:32:05 +0100

    Add coloring based on groups

--- backend/src/Simulation/simulation_runner.py
+++ backend/src/Simulation/simulation_runner.py
@@ -19,6 +19,7 @@ class SimulationRunner:
         tracemalloc.start()
 
         self.db_connector.delete_previous_simulation_of_socket(self.socket_id)
+        await self.send_groups_to_socket(self.model.website.friend_links.groups)
         for i in range(self.number_of_steps):
             print("--------------------" + str(i) + "----------------")
             self.__iterations += 1
@@ -47,4 +48,16 @@ class SimulationRunner:
         }
         await self.socket_server.emit(
             "simulation_step_finished", data_to_send, room=self.socket_id
+        )
+    
+    async def send_groups_to_socket(self, groups_data):
+        # Reformat to make dictionary, where key: agent_id, value: group_of_agent
+        data = {}
+        data["groups"] = {}
+        for (key, value) in groups_data.items():
+            for id in value:
+                data["groups"][id] = key
+        data["group_count"] = len(groups_data.keys())
+        await self.socket_server.emit(
+            "groups_for_simulation_sent", data, room=self.socket_id
         )
